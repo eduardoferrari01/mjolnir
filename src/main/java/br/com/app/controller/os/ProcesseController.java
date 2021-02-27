0927a04884cb1182fad56b9ff6efdcda42a1b254
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-02-27 14:59:17 -0300

    Adicionado RestController e  RequestMapping

--- src/main/java/br/com/app/controller/os/ProcesseController.java
+++ src/main/java/br/com/app/controller/os/ProcesseController.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.app.domain.ColetaResultado;
 
-public class OSProcessController {
+@RestController
+@RequestMapping("/api/processe")
+public class ProcesseController {
 
-	private Logger logger = LoggerFactory.getLogger(OSProcessController.class);
+	private Logger logger = LoggerFactory.getLogger(ProcesseController.class);
 	
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
