011db51a73441530b10f6f10fbaa2f5f5d01e9bc
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-05-09 15:29:48 -0300

    Teste unitário para ComputerSystem

--- src/test/java/br/com/app/service/hardware/ComputerSystemTest.java
+++ src/test/java/br/com/app/service/hardware/ComputerSystemTest.java
@@ -1,9 +1,13 @@
 package br.com.app.service.hardware;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +17,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.app.domain.ColetaResultado;
+import br.com.app.domain.NotFoundException;
+import br.com.app.domain.builder.hardware.ComputerSystemBuilder;
 import br.com.app.domain.hardware.ComputerSystem;
 import br.com.app.repository.hardware.ComputerSystemRepository;
 import br.com.app.test.builder.ColetaResultadoTestDataBuilder;
@@ -41,4 +47,27 @@ public class ComputerSystemTest {
 		computerSystemService.save(coletaResultado);
 		verify(computerSystemRepository).save(Mockito.any(ComputerSystem.class));
 	}
+	
+	@Test
+	public void deveRetornarUmComputerSystem() {
+
+		ComputerSystem computerSystem = new ComputerSystemBuilder(coletaResultado).builder();
+
+		when(computerSystemRepository.findById(coletaResultado.getId())).thenReturn(Optional.of(new ComputerSystemBuilder(coletaResultado).builder()));
+
+		ComputerSystem computerSystemRetorno = computerSystemService.findById(coletaResultado.getId());
+		verify(computerSystemRepository).findById(coletaResultado.getId());
+
+		Assertions.assertEquals(computerSystemRetorno.getId(), computerSystem.getId());
+	}
+	
+	
+	@Test
+	public void deveLancarNotFoundExceptionQuandoIdNaoExistir() {
+		
+		assertThrows(NotFoundException.class, () -> {
+			computerSystemService.findById("123");
+		});
+	}
+	
 }
