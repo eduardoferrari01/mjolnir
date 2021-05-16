0313de201a3fd0b69d8810624cae2ee8104ebac3
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-05-16 18:06:06 -0300

    Teste unitário que valida MemoryBuilder

--- src/test/java/br/com/app/service/hardware/MemoryTest.java
+++ src/test/java/br/com/app/service/hardware/MemoryTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +14,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.app.domain.ColetaResultado;
-import br.com.app.domain.hardware.GraphicsCards;
+import br.com.app.domain.builder.hardware.MemoryBuilder;
 import br.com.app.domain.hardware.Memory;
 import br.com.app.repository.hardware.MemoryRepository;
 import br.com.app.test.builder.ColetaResultadoTestDataBuilder;
@@ -40,5 +41,25 @@ public class MemoryTest {
 
 		memoryService.save(coletaResultado);
 		verify(memoryRepository).save(Mockito.any(Memory.class));
+	}
+	
+	@Test
+	public void deveCriarUmMemory() {
+		
+		Memory memory = new MemoryBuilder(coletaResultado).builder();
+		
+		Assertions.assertNotNull(memory);
+		Assertions.assertEquals(coletaResultado.getId(), memory.getId());
+		Assertions.assertEquals(8038240256L, memory.getTotal());
+		Assertions.assertEquals(2298691584L, memory.getAvailable());
+		Assertions.assertEquals(4096L, memory.getPageSize());
+		Assertions.assertNotNull(memory.getVirtualMemory());
+		Assertions.assertEquals(8450203648L ,memory.getVirtualMemory().getSwapTotal());
+		Assertions.assertEquals(4194304L ,memory.getVirtualMemory().getSwapUsed());
+		Assertions.assertEquals(12469321728L ,memory.getVirtualMemory().getVirtualMax());
+		Assertions.assertEquals(5743742976L ,memory.getVirtualMemory().getVirtualInUse());
+		Assertions.assertEquals(0 ,memory.getVirtualMemory().getSwapPagesIn());
+		Assertions.assertEquals(8168L ,memory.getVirtualMemory().getSwapPagesOut());
+		Assertions.assertNotNull(memory.getPhysicalMemory());
 	}
 }
