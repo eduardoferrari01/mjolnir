2122cd9e958ad4a46f4f7db139ca18b17f9d886a
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-05-27 18:40:17 -0300

    Implementado o método de teste deveConstruirSensor

--- src/test/java/br/com/app/service/hardware/SensorTest.java
+++ src/test/java/br/com/app/service/hardware/SensorTest.java
@@ -3,7 +3,9 @@ package br.com.app.service.hardware;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.app.domain.ColetaResultado;
+import br.com.app.domain.builder.hardware.SensorBuilder;
 import br.com.app.domain.hardware.Sensor;
 import br.com.app.repository.hardware.SensorRepository;
 import br.com.app.test.builder.ColetaResultadoTestDataBuilder;
@@ -39,5 +42,18 @@ public class SensorTest {
 
 		sensorService.save(coletaResultado);
 		verify(sensorRepository).save(Mockito.any(Sensor.class));
+	}
+	
+	@Test
+	public void deveConstruirSensor(){
+		
+		Sensor sensor = new SensorBuilder(coletaResultado).builder();
+		
+		Assertions.assertNotNull(sensor);
+		Assertions.assertEquals(coletaResultado.getId(), sensor.getId());
+		Assertions.assertEquals(47.0, sensor.getCpuTemperature());
+		Assertions.assertEquals(0.0, sensor.getCpuVoltage());
+		Assertions.assertFalse(sensor.getFanSpeeds().isEmpty());
+		Assertions.assertEquals(Arrays.asList(50, 10), sensor.getFanSpeeds());
 	}
 }
