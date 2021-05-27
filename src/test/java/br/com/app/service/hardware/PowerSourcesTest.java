0c1adfadb666dcadd4c6edab565a1419f4dbe8ad
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-05-26 22:32:22 -0300

    Refatoração da classe de teste PowerSourcesTest

--- src/test/java/br/com/app/service/hardware/PowerSourcesTest.java
+++ src/test/java/br/com/app/service/hardware/PowerSourcesTest.java
@@ -3,7 +3,9 @@ package br.com.app.service.hardware;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +15,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.app.domain.ColetaResultado;
+import br.com.app.domain.builder.hardware.PowerSourcesBuilder;
+import br.com.app.domain.enums.CapacityUnits;
+import br.com.app.domain.hardware.PowerSource;
 import br.com.app.domain.hardware.PowerSources;
 import br.com.app.repository.hardware.PowerSourcesRepository;
 import br.com.app.test.builder.ColetaResultadoTestDataBuilder;
@@ -39,6 +44,39 @@ public class PowerSourcesTest {
 
 		powerSourcesService.save(coletaResultado);
 		verify(powerSourcesRepository).save(Mockito.any(PowerSources.class));
+	}
+	
+	@Test
+	public void deveRetornarUmPowerSources(){
+	
+		PowerSources powerSources = new PowerSourcesBuilder(coletaResultado).builder();
+		
+		Assertions.assertNotNull(powerSources);
+		Assertions.assertEquals(coletaResultado.getId(), powerSources.getId());
+		Assertions.assertFalse(powerSources.getPowerSources().isEmpty());
+	
+		PowerSource powerSource = powerSources.getPowerSources().get(0);
+		Assertions.assertEquals("hidpp_battery_0", powerSource.getName());
+		Assertions.assertEquals("Wireless Keyboard", powerSource.getDeviceName());
+		Assertions.assertEquals(-1.0, powerSource.getRemainingCapacityPercent());
+		Assertions.assertEquals(-1.0, powerSource.getTimeRemainingEstimated());
+		Assertions.assertEquals(-1.0, powerSource.getTimeRemainingInstant());
+		Assertions.assertEquals(0.0, powerSource.getPowerUsageRate());
+		Assertions.assertEquals(-1.0, powerSource.getVoltage());
+		Assertions.assertEquals(0.0, powerSource.getAmperage());
+		Assertions.assertEquals(Boolean.FALSE, powerSource.getPowerOnLine());
+		Assertions.assertEquals(Boolean.FALSE, powerSource.getCharging());
+		Assertions.assertEquals(Boolean.TRUE, powerSource.getDischarging());
+		Assertions.assertEquals(CapacityUnits.RELATIVE, powerSource.getCapacityUnits());
+		Assertions.assertEquals(-1, powerSource.getCurrentCapacity());
+		Assertions.assertEquals(-1, powerSource.getMaxCapacity());
+		Assertions.assertEquals(-1, powerSource.getDesignCapacity());
+		Assertions.assertEquals(-1, powerSource.getCycleCount());
+		Assertions.assertEquals("unknown", powerSource.getChemistry());
+		Assertions.assertEquals(LocalDate.of(2021, 02, 01), powerSource.getManufactureDate());
+		Assertions.assertEquals("Logitech", powerSource.getManufacturer());
+		Assertions.assertEquals("4023-00-00-00-00", powerSource.getSerialNumber());
+		Assertions.assertEquals(0.0, powerSource.getTemperature());
 	}
 	
 }
