bc0c345ccf926a593481662af8a2c3d385a8f1fe
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-06-05 15:18:15 -0300

    Adicionado response status no DiskStoresController

--- src/main/java/br/com/app/controller/hardware/DiskStoresController.java
+++ src/main/java/br/com/app/controller/hardware/DiskStoresController.java
@@ -3,9 +3,11 @@ package br.com.app.controller.hardware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.app.domain.ColetaResultado;
@@ -16,10 +18,10 @@ import br.com.app.service.hardware.DisksService;
 public class DiskStoresController {
 
 	private Logger logger = LoggerFactory.getLogger(DiskStoresController.class);
-	
 	@Autowired
 	private DisksService disksService;
 	
+	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
 	public void post(@RequestBody ColetaResultado coletaResultado) {
 
