5e6b3b13abbd6b16bed087ef45af7039c5154cf5
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-06-05 15:50:23 -0300

    Adicionado ResponseStatus(HttpStatus.CREATED)

--- src/main/java/br/com/app/controller/hardware/GraphicsCardsController.java
+++ src/main/java/br/com/app/controller/hardware/GraphicsCardsController.java
@@ -3,9 +3,11 @@ package br.com.app.controller.hardware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.app.domain.ColetaResultado;
@@ -20,6 +22,7 @@ public class GraphicsCardsController {
 	@Autowired
 	private GraphicsCardsService graphicsCardsService;
 
+	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
 	public void post(@RequestBody ColetaResultado coletaResultado) {
 
